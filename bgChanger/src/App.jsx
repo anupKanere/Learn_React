6919e932c7e0be12cae645019f9a2a4e54f07ad9
anupKanere <kanereanup@gmail.com>
2023-10-03 17:34:34 +0530

    Project-01 Background colour change

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,36 @@
+import { red } from "@mui/material/colors"
+import { useState } from "react"
+
+function App() {
+  const [color, setColor] = useState("skyblue")
+
+  return (
+    
+      <div className="w-full h-screen duration-200 " style={{ backgroundColor: color }}>
+
+        <div className="fixed flex flex-wrap justify-center top-12 inset-x-0 px-2">
+
+          <div className="flex flex-wrap justify-center  gap-4 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button onClick={() => setColor("red")} className="outline-none px-5 py-2 rounded-xl text-white shadow-lg " style={{backgroundColor: "red"}}>Red</button>
+
+            <button onClick={() => setColor("green")} className="outline-none rounded-xl bg-green-500 px-2 py-2">Green</button>
+
+            <button onClick={() => setColor("blue")} className="outline-none rounded-xl bg-blue-700 px-2 py-2">Navy Blue</button>
+
+            <button onClick={() => setColor("lime")}  className="outline-none rounded-xl bg-lime-300 px-2 py-2 text-black">Lime</button>
+
+            <button onClick={() => setColor("violet")}  className="outline-none rounded-xl bg-violet-500 px-2 py-2">Violet</button>
+
+            <button onClick={() => setColor("orange")}  className="outline-none rounded-xl bg-orange-500 px-2 py-2">Orange</button>
+          
+          </div>
+
+
+        </div>
+
+      </div>
+    
+  )
+}
+
+export default App
